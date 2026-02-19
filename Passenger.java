33169e5d35f9cf204350fefc9b174bd9613c7044
Mira Mavis <mandmvoice@gmail.com>
2026-02-19 10:22:43 -0800

    finished the getters and setters in passenger

--- Passenger.java
+++ Passenger.java
@@ -1,12 +1,79 @@
+import java.util.*;
+
 public class Passenger{
 
     private int passNum;
-    private int passGen;
+    private int passGen = 1;
     private int currentStation;
     private int startingStation;
-    private int destination;
+    private int destination; //false for backwards true for forward
     private boolean direction;
     private boolean inCar;
     private boolean atDestination;
+
+    public Passenger(){
+        passNum = passGen;
+        passGen ++;
+        startingStation = (int)((Math.random()*31)+ 1);
+        destination = (int)((Math.random()*31)+ 1);
+        currentStation = startingStation;
+        inCar = false;
+        
+        if (startingStation > destination){
+            direction = false;
+        }
+        
+        if (startingStation <= destination){
+            direction = true;
+        }
+
+        if (currentStation == destination){
+            atDestination = true;
+        }
+        else{
+            atDestination = false;
+        }
+
+    }
+
+    //getters 
+    public int getPassengerName(){
+        return passNum;
+    }
+
+    public int getCurrentStation(){
+        return currentStation;
+    }
+
+    public int getDestination(){
+        return destination;
+    }
+
+    public boolean getDirection(){
+        return direction;
+    }
+
+    public boolean getAtDestination(){
+        return atDestination;
+    }
+
+    public boolean getinCar(){
+        return inCar;
+    }
+
+    //setters
+    public void setIsAtDestination(){
+        if (currentStation == destination){
+            atDestination = true;
+        }
+        else{
+            atDestination = false;
+        }
+    }
+
+    public void setCurrentStation(int newStation){
+        currentStation = newStation;
+    }
+
 
 }
