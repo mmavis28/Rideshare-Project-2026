472572f115e4472213dace892cda6c4ab282aa55
Mira Mavis <mandmvoice@gmail.com>
2026-02-22 17:57:14 -0800

    created Station toString

--- Station.java
+++ Station.java
@@ -5,11 +5,13 @@ public class Station {
     private int stationNum;
     private ArrayList<Passenger> waiting;
     private ArrayList<Passenger> arrived;
+    private ArrayList<Car> carsAtStation;
 
     public Station(int stationNumber){
         stationNum = stationNumber;
         waiting = new ArrayList<Passenger>();
         arrived = new ArrayList<Passenger>();
+        carsAtStation = new ArrayList<Car>();
     }
 
     //getters
@@ -26,12 +28,20 @@ public class Station {
         return arrived;
     }
 
+    public ArrayList<Car> getCarsAtStation(){
+        return carsAtStation;
+    }
+
     public int numWaiting(){
         return waiting.size();
     }
 
     public int numArrived(){
         return arrived.size();
+    }
+
+    private int numCarsAtStation(){
+        return carsAtStation.size();
     }
 
     //setters
@@ -51,8 +61,24 @@ public class Station {
     //toString
 
     public String toString(){
-        return "Station " + stationNum + ": Waiting = " + waiting.size() + ", Arrived = " + arrived.size() + ".";
-    }
+        String s = "Station " + stationNum + ": \n";
+        s += "  Waiting Passengers: (" + numWaiting() + ") \n";
+        for (int i = 0; i < numWaiting(); i++){
+            s += "      " + waiting.get(i).toString() + "\n";
+        }
+        s += "  Arrived Passengers: (" + numArrived() + ") \n";
+        for (int i = 0; i < numArrived(); i++){
+            s += "      " + arrived.get(i).toString() + "\n";
+        }
 
+        s += "  Cars at Station: (" + numCarsAtStation() + ") \n";
+        for (int i = 0; i < numCarsAtStation(); i++){
+            s += "      " + carsAtStation.get(i).toString() + "\n";
+        }
+
+        return s;
+    }
+    
+    //print out all passengers 
 
 }
